0d48b4af30f83dc699d4cfe14b1b4360d14c23e3
johnbrian <johnbriancapate@gmail.com>
2022-11-15 06:06:12 +0800

    Add ENV and change gitignore config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -14,14 +15,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
+const secret = process.env.SECRET;
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-const secret = "thisisthesecretkeyiguess";
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"], decryptPostSave: false });
 
 const User = new mongoose.model("User", userSchema); //ALWAYS PUT THIS AFTER SETTING UP A ENCRYPTION
 
